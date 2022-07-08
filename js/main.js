ee47e9881c728df1e37c5604fe8ac9739b82dd1c
fish-base <zqzlgzh@qq.com>
2022-07-08 13:27:22 +0800

    modify

--- js/main.js
+++ js/main.js
@@ -1,4 +1,94 @@
 
+
+/**    菜单栏上滑显示  下滑 消失 ** */
+
+let t1 = 0;
+let t2 = 0;
+let timer = null; // 定时器
+
+/** 节流 */
+let scrolling = false;
+
+document.onscroll = function() {
+    scrolling = true;
+
+}
+let t3 = 0;
+setInterval( function(){
+   
+    if(scrolling){
+        scrolling = false;
+        
+        clearTimeout(timer);    
+       t1 = document.documentElement.scrollTop || document.body.scrollTop;      
+       timer = setTimeout(isScrollEnd, 200);  
+    }
+    }
+,200)
+/** 
+function isScrollEnd() {
+    t2 = document.documentElement.scrollTop || document.body.scrollTop;
+    let head_1 = document.getElementById('nav_button');
+
+    /**if(t2 > t3){        
+    console.log(head_1)
+      console.log(t3,t2)
+      t3 = t2;
+            if(t3>100){        
+                console.log(head_1.style.display)
+                head_1.style.visibility = 'hidden';
+                
+                console.log('no -display')
+                }
+    }else {
+        console.log('上')
+        console.log(t3,t2) 
+        t3 = t2;
+            if(t3>100){
+            
+            console.log(head_1.style.display)
+            head_1.style.visibility = 'visible';
+            
+            console.log('display')
+            }
+    }**/
+  
+
+  function isScrollEnd(){
+    t2 = document.documentElement.scrollTop || document.body.scrollTop;
+    let head_1 = document.getElementById('nav_button');
+
+    if(t3>100 || t2>100){
+        if(t2 > t3){
+            t3 = t2;
+            head_1.style.visibility = 'hidden';
+        }else {
+            t3 = t2;
+            head_1.style.visibility = 'visible';
+    }
+            }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*   **    * */
+
+
+
 $(function () {
     click_small_btn();
     header_search_btn();
@@ -22,4 +112,5 @@ function click_small_btn() {
         });
     });
 }
+
 
